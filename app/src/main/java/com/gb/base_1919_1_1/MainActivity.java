50145964fc49a27faae35244535f268c5d755567
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-20 21:42:43 +0300

    Урок 1 разобрались с minSdk, targetSdk, compileSdk

--- app/src/main/java/com/gb/base_1919_1_1/MainActivity.java
+++ app/src/main/java/com/gb/base_1919_1_1/MainActivity.java
@@ -2,6 +2,8 @@ package com.gb.base_1919_1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // совесть разработчика
+            startForegroundService(new Intent(this,MyService.class));
+        }else {
+            // КАКИЕ-ТО КОСТЫЛИ startService
+        }
     }
 }
