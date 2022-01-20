74bb6e0b715ff8ee2654bb50ca45926b6310d92b
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-20 23:59:50 +0300

    Урок 1 закончили

--- app/src/main/java/com/gb/base_1919_1_1/MainActivity.java
+++ app/src/main/java/com/gb/base_1919_1_1/MainActivity.java
@@ -2,10 +2,12 @@ package com.gb.base_1919_1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +16,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("mylogs","MainActivity onCreate()");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // совесть разработчика
-            startForegroundService(new Intent(this,SpyService.class));
-        }else {
-            // КАКИЕ-ТО КОСТЫЛИ startService
+
+
+        Button button = findViewById(R.id.btn);
+        EditText etOne= findViewById(R.id.etOne);
+        EditText etTwo=findViewById(R.id.etTwo);
+        TextView answer=findViewById(R.id.answer);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("mylogs","onClick");
+                compare(etOne, etTwo, answer);
+            }
+        });
+    }
+
+    public void compare(EditText etOne, EditText etTwo, TextView answer) {
+        int one = Integer.valueOf(etOne.getText().toString());
+        int two = Integer.valueOf(etTwo.getText().toString());
+        if(one==two){
+            answer.setText("Равно!");
+        }else{
+            answer.setText("не Равно!");
         }
     }
+
+
 }
